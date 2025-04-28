515e22b3a1cdb0cc5b1f940dfffb672550e9b65e
zzaiyan <zzaiyan@outlook.com>
2025-04-29 04:22:42 +0800

    update github url.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='TouchHook: A PyTorch hook management library',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/torchhook',
+    url='https://github.com/zzaiyan/TorchHook',
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
