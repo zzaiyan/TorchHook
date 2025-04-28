b2578fa006bdd0d629b78c69ef0263f9c223252d
zzaiyan <zzaiyan@outlook.com>
2025-04-29 06:05:05 +0800

    fix publish.yml

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='torchhook',
-    version='0.1.6',
+    version='0.1.7',
     author='Zaiyan Zhang',
     author_email='1@zzaiyan.com',
     description='TouchHook: A PyTorch hook management library',
